778b61388be594754520a27f39910e754df81474
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 22:32:59 -0600

    added quiz

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-{\rtf1}
+var answers = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A"], 
+    tot = answers.length;
+
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
+}
+
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+  return score;
+}
+
+function returnScore(){
+  alert("Your score is "+ getScore() +"/"+ tot);
+}
